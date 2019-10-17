06a5420cee766dd0951b254eff30c855f9c2f511
Tomás R. Pita <tomasrpita@gmail.com>
2019-10-17 07:01:15 +0200

    ft_strncmp.c

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -13,22 +13,16 @@
 int		ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
-	unsigned int	result;
 
 	i = 0;
-	while (s1 && s2 && n != 0 && i < n)
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
 	{
 		if (s1[i] != s2[i])
-		{
-			result = (unsigned char)s1[i] - (unsigned char)s2[i];
-			return (result);
-		}
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	if (s1[i] != s2[i] && i != n)
-	{
-		result = (unsigned char)s1[i] - (unsigned char)s2[i];
-		return (result);
-	}
+	if (i < n && ((s1[i] == '\0' && s2[i] != '\0') ||
+				(s2[i] == '\0' && s1[i] != '\0')))
+		return (s1[i] - s2[i]);
 	return (0);
 }
